ed8acf3744be4e96ee607c90d6a18d7c5686186b
Roberto Alencar <rroberto.alencar@gmail.com>
2023-09-13 11:12:07 -0300

    Enviando arquivos do projeto

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 
 import FormCliente from './views/cliente/FormCliente';
+import ListCliente from './views/cliente/ListCliente';
 import Home from './views/home/Home';
 
 function Rotas() {
@@ -9,6 +10,7 @@ function Rotas() {
         <Routes>
             <Route path="/" element={ <Home/> } />
             <Route path="form-cliente" element={ <FormCliente/> } />
+            <Route path="list-cliente" element={ <ListCliente/> } />
         </Routes>
     )
 }
